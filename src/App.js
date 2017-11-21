daa7da11dccf2a4f283b73638768d90ef9370dec
jalil wahdatehagh <j.wahdatehagh@gmail.com>
2017-11-21 01:19:13 +0100

    Prepare bulma for later customization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
-import { Button } from 'reactbulma'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button } from './bulma'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Button primary>Hehe</Button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default App
