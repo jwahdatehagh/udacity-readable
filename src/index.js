89cbd30892383c21bd205f8ee7eb8365468f6db9
jalil wahdatehagh <j.wahdatehagh@gmail.com>
2017-11-26 10:21:55 +0100

    Fix EsLint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 
-import bulma from './bulma'
+import './bulma'
 import App from './App'
 import store from './store'
 
